7e6a33c99e9af3bf7a5c1e54893a4824c21610b4
Martin_Ingham <martiningham@inghamconsulting.co.uk>
2015-11-05 14:25:16 +0000

    Add/remove order.

--- LiveOrderBoard/LiveOrderBoard.cs
+++ LiveOrderBoard/LiveOrderBoard.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,24 @@ namespace CodeExercise
 {
     public class LiveOrderBoard
     {
-        public void RegisterOrder(Order order)
+        private IDictionary<Guid, Order> orders = new Dictionary<Guid, Order>();
+
+        public Guid RegisterOrder(Order order)
         {
+            var orderId = Guid.NewGuid();
+            // Assume we can absolutely guarantee a GUID will be unique.
+            orders.Add(orderId, order);
+            return orderId;
+        }
+
+        public void CancelRegisteredOrder(Guid orderId)
+        {
+            orders.Remove(orderId);
+        }
+
+        public IEnumerable GetOrders()
+        {
+            return orders.Values;
         }
     }
 }
--- UnitTestProject1/LiveOrderBoardTests.cs
+++ UnitTestProject1/LiveOrderBoardTests.cs
@@ -15,15 +15,45 @@ namespace CodeExercise.Tests
         }
 
         [Test]
-        public void RegisterOrder_Should_Accept_Order_Parameter()
+        public void RegisterOrder_Should_Add_Order()
         {
             // Arrange
             var order = new Order("Test", "3.5 kg", "£303", OrderType.Buy);
 
             // Act
-            board.RegisterOrder(order);
+            var result = board.RegisterOrder(order);
             
             // Assert
+            Assert.IsNotEmpty(board.GetOrders());
+        }
+
+        [Test]
+        public void CancelRegisteredOrder_Should_Remove_Order_If_It_Matches_Id()
+        {
+            // Arrange
+            var order = new Order("Test", "3.5 kg", "£303", OrderType.Buy);
+            var orderId = board.RegisterOrder(order);
+
+            // Act
+            board.CancelRegisteredOrder(orderId);
+
+            // Assert
+            Assert.IsEmpty(board.GetOrders());
+        }
+
+        [Test]
+        public void CancelRegisteredOrder_Should_Not_Remove_Order_If_It_Does_Not_Match_Id()
+        {
+            // Arrange
+            var order = new Order("Test", "3.5 kg", "£303", OrderType.Buy);
+            var id1 = board.RegisterOrder(order);
+            var id2 = Guid.NewGuid();
+
+            // Act
+            board.CancelRegisteredOrder(id2);
+
+            // Assert
+            Assert.IsNotEmpty(board.GetOrders());
         }
     }
 }
